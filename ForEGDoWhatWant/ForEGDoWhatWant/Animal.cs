dca27741cf5a59c4f2a1071a525e40de22355f0f
Valyan <161570222+Valentine-doc@users.noreply.github.com>
2024-10-18 17:24:10 +0300

    Project
    Для работы с проектом

---
+++ ForEGDoWhatWant/ForEGDoWhatWant/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEGDoWhatWant
+{
+    public class Animal
+    {
+        public void Sound()
+        {
+            Console.WriteLine("Животное издает звуки");
+        }
+        public void ReactToCar()
+        {
+            Console.WriteLine("Реагирует на машину");
+        }
+    }
+}
+
