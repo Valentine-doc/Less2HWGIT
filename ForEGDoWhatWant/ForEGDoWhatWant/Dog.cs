3655edc0d72803eea79a8b356c3663413e028a9f
Valyan <161570222+Valentine-doc@users.noreply.github.com>
2024-10-18 17:33:15 +0300

    Project
    Для работы и правок

---
+++ ForEGDoWhatWant/ForEGDoWhatWant/Dog.cs
@@ -0,0 +1,71 @@
+﻿using System.Xml.Linq;
+
+namespace ForEGDoWhatWant
+{
+    public class Dog : Animal   // класс Dog наследник класса Animal
+    {
+        public int age;   // public сделано для того, чтобы бы ОБРАТИТЬСЯ, то есть в классе puppy мы можем обратиться к breed
+
+        private string breed;   // privat сделано для того, чтобы НЕ ОБРАТИТЬСЯ, то есть в классе puppy мы не можем обратиться к breed
+
+        public string Name {  get; set; }
+
+
+
+
+        public virtual void ReactToCar()
+        {
+            Console.WriteLine("Dog barks at the car");
+        }
+
+        public virtual void ReactToPet()
+        {
+            Console.WriteLine("Собака виляет хвостом");
+        }
+
+
+        public int Age  // свойства полей age и breed
+        {
+            get { return age; }     // get - получить
+            set {
+                    if(value<1)
+                    {
+                        Console.WriteLine("Возраст не может быть меньше чем 1");
+                        age = 1;
+                    }
+                    else
+                    {
+                        age = value;
+                    }
+                }    // set- установить
+        }
+
+        public string Breed
+        {
+            get { return breed; }   // get 
+            set { breed = value; }  // set 
+        }
+
+        public Dog(string name, int age, string breed)
+        {
+            this.Name = "Петя";
+            this.Age = 4;
+            this.Breed = "Бульдог";
+        }
+
+
+        public Dog()
+        {
+        }
+
+        public void DisplayInfo()
+        {
+            Name = "Петя";
+            Age = 4;
+            Breed = "Бульдог";
+        }
+
+    }
+}
+
+
