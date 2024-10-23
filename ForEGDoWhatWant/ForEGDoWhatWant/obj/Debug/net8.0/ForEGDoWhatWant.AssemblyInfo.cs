2fdff6657da57cd1594c690915de30fd3e424030
valyan <vally-vlabo@yandex.ru>
2024-10-23 21:41:37 +0300

    git clone

--- ForEGDoWhatWant/ForEGDoWhatWant/obj/Debug/net8.0/ForEGDoWhatWant.AssemblyInfo.cs
+++ ForEGDoWhatWant/ForEGDoWhatWant/obj/Debug/net8.0/ForEGDoWhatWant.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ForEGDoWhatWant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5acf7c85f22a9e1647bde8411d40b5f86e21eb01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c139b4ea6b199c2c9df01506973fa43a2355fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ForEGDoWhatWant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ForEGDoWhatWant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
